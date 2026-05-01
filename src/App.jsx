6ca5315f9692f251d14c28400adbfddd194780d0
Awena Bachu Farouq <bachufarouqawena@gmail.com>
2026-05-01 20:20:04 +0300

    app and inde jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,25 @@
+import { useEffect } from "react";
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
 
+function App() {
+  useEffect(() => {
+      async function loadWebsites() {
+            const snapshot = await getDocs(collection(db, "websites"));
+
+                  snapshot.forEach((doc) => {
+                          console.log(doc.data());
+                                });
+                                    }
+
+                                        loadWebsites();
+                                          }, []);
+
+                                            return (
+                                                <div>
+                                                      <h1>UsefulHub</h1>
+                                                          </div>
+                                                            );
+                                                            }
+
+                                                            export default App;
